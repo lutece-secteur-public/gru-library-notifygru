d61c16069dd0028e653fc2196f7d3cf97625b52f
MOIRE <cyril.moire@atos.net>
2016-05-24 17:14:29 +0200

    formatting

--- src/java/fr/paris/lutece/plugins/librarynotifygru/business/notifygru/NotifyGruGuichetNotification.java
+++ src/java/fr/paris/lutece/plugins/librarynotifygru/business/notifygru/NotifyGruGuichetNotification.java
@@ -33,12 +33,11 @@
  */
 package fr.paris.lutece.plugins.librarynotifygru.business.notifygru;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import fr.paris.lutece.plugins.librarynotifygru.constant.ConstantsLibraryNotifyGru;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 // TODO: Auto-generated Javadoc
