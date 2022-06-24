662cc4d8429f91fab099ef37d28c35ebe8d5cec7
sebastien leridon <sebleridon@laposte.net>
2022-06-24 10:34:06 +0200

    GRULIBNOTI-29 : Fix notification service call api authentication type

--- src/java/fr/paris/lutece/plugins/librarynotifygru/rs/service/NotificationTransportApiManagerRest.java
+++ src/java/fr/paris/lutece/plugins/librarynotifygru/rs/service/NotificationTransportApiManagerRest.java
@@ -57,7 +57,8 @@ public final class NotificationTransportApiManagerRest extends AbstractNotificat
 {
     /** The Constant PARAMS_ACCES_TOKEN. */
     private static final String PARAMS_ACCES_TOKEN = "access_token";
-    private static final String TYPE_AUTHENTIFICATION = "Bearer";
+    private static final String TYPE_AUTHENTIFICATION_BEARER = "Bearer";
+    private static final String TYPE_AUTHENTIFICATION_BASIC = "Basic";
 
     /** The Constant PARAMS_GRANT_TYPE. */
     private static final String PARAMS_GRANT_TYPE = "grant_type";
@@ -119,7 +120,7 @@ public final class NotificationTransportApiManagerRest extends AbstractNotificat
 
         mapHeadersRequest.put( HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON );
         mapHeadersRequest.put( HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED );
-        mapHeadersRequest.put( HttpHeaders.AUTHORIZATION, TYPE_AUTHENTIFICATION + " " + _strApiManagerCredentials );
+        mapHeadersRequest.put( HttpHeaders.AUTHORIZATION, TYPE_AUTHENTIFICATION_BASIC + " " + _strApiManagerCredentials );
 
         String strJson = getHttpTransport( ).doPost( _strApiManagerEndPoint, mapParams, mapHeadersRequest );
 
@@ -151,7 +152,7 @@ public final class NotificationTransportApiManagerRest extends AbstractNotificat
 
         if ( StringUtils.isNotBlank( strToken ) )
         {
-            mapHeadersRequest.put( HttpHeaders.AUTHORIZATION, TYPE_AUTHENTIFICATION + " " + strToken );
+            mapHeadersRequest.put( HttpHeaders.AUTHORIZATION, TYPE_AUTHENTIFICATION_BEARER + " " + strToken );
         }
     }
 }
